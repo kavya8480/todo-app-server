e12575447b19e15423903697329971d323f39d08
kavya8480 <tapaswi84802@gmail.com>
2026-04-17 11:51:42 +0530

    added files

--- controller/authController.js
+++
@@ -1,8 +0,0 @@
- async function login(req,res){
-console.log("Inside login controller.");
-console.log("Got the request body for the api call",req.body);
-console.log("Got the request header for the api call",req.headers['content-type']);
-res.send(req.body);
-}
-
-module.exports = login;
